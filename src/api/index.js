509369d776d89869f802cba30551d987af23a0b4
Diego Sanchez <diegosanchez@Diegos-MacBook-Pro.local>
2021-05-26 12:26:38 -0500

    deployment fixes

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import dotenv from 'dotenv'
 
-
-const url = 'http://localhost:5000/posts';
+const URL = process.env.URL
+const url = URL
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost)
